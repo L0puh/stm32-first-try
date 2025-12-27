d230ea34b14fe22b4b66d45567281433c29054b6
L0puh <1opuhart@gmail.com>
2025-12-27 12:37:58 +0300

    update

--- status_blink/main.c
+++ status_blink/main.c
@@ -1,24 +1,33 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 
-#define LED_PIN GPIO13
-
-static void gpio_setup(void){
-   rcc_periph_clock_enable(RCC_GPIOC);
-   gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_2_MHZ, 
-                  GPIO_CNF_OUTPUT_PUSHPULL, LED_PIN);
-            
+static void delay(volatile uint32_t count)
+{
+    while (count--) {
+        __asm__ volatile ("nop");
+    }
 }
 
-void delay(int ms) {
-   for (int i = 0; i < ms; i++){
-      __asm__("nop");
-   }
+int main(void)
+{
+    /* Enable GPIOC clock */
+    rcc_periph_clock_enable(RCC_GPIOC);
+
+    /* PC13: LED (active LOW on Blue Pill) */
+    gpio_set_mode(GPIOC,
+                  GPIO_MODE_OUTPUT_2_MHZ,
+                  GPIO_CNF_OUTPUT_PUSHPULL,
+                  GPIO13);
+
+    /* LED OFF initially */
+    gpio_set(GPIOC, GPIO13);
+
+    while (1) {
+        gpio_clear(GPIOC, GPIO13);  /* LED ON */
+        delay(1000000);
+
+        gpio_set(GPIOC, GPIO13);    /* LED OFF */
+        delay(1000000);
+    }
 }
 
-int main(void) {
-   gpio_setup();
-   while(1){
-      gpio_clear(GPIOC, LED_PIN);
-   }
-}
